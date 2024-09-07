1c5f040ca044ae28c2de5e015e309061677743f1
Tuananh <tuananh48218@gmail.com>
2024-09-07 16:33:29 +0700

    Cài đặt và cấu hình Router | Xây dựng cơ chế tải Layout cho dự án Tiktok Clone

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+// Layout
+import { HeaderOnly } from "~/components/Layout";
+
+import Home from "~/pages/Home";
+import Following from "~/pages/Following";
+import Upload from "~/pages/Upload";
+
+// Public
+const publicRoutes = [
+    { path: "/", component: Home },
+    { path: "/following", component: Following },
+    { path: "/upload", component: Upload, layout: HeaderOnly },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
